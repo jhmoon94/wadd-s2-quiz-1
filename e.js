28073be0d2b0b621ddce916b1a2c393771ecedc4
jhmoon94 <jhmoon94@yahoo.com>
2021-02-08 13:26:24 -0800

    solution

--- e.js
+++ e.js
@@ -11,7 +11,12 @@ function e() {
   // remember to call .toString()
   // from records where their `first_name` starts with "po" case insensitive
   // ordered by their first_name in desc order
-
+  return knex
+  .select('first_name', 'last_name', 'created_at')
+  .from('students')
+  .where('first_name', 'ilike', 'po%')
+  .orderBy('first_name', 'desc')
+  .toString();
 
 }
 
